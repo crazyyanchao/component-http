476b12523c02b4d98c627fa8ea67e3b10163dcc6
crazyyanchao <1141679423@qq.com>
2019-12-18 18:22:26 +0800

    Initial commit

---
+++ src/main/java/casia/isi/component/http/StatusResponseHandler.java
@@ -0,0 +1,19 @@
+package casia.isi.component.http;
+
+import org.apache.http.HttpResponse;
+
+public abstract class StatusResponseHandler {
+	protected int reponseStatus;
+	public int getReponseStatus() {
+		return reponseStatus;
+	}
+
+	public void setReponseStatus(int reponseStatus) {
+		this.reponseStatus = reponseStatus;
+	}
+
+	protected int initStatus(HttpResponse response){
+		reponseStatus = response.getStatusLine().getStatusCode();
+		return reponseStatus;
+	}
+}
